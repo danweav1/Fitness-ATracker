866e84836c1a5420f55479b9c64e5c0051dc2b6c
danweaver12 <danweaver12@gmail.com>
2020-04-26 19:20:02 +0200

    Mostly finished app. Authentication works with firebase. No errors currently. Had to revert angular cli to older version for the moment.

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyARlIYOvaed9A0mWphNFv5Usf18hjvIhgM",
+    authDomain: "fitness-tracker-980c7.firebaseapp.com",
+    databaseURL: "https://fitness-tracker-980c7.firebaseio.com",
+    projectId: "fitness-tracker-980c7",
+    storageBucket: "fitness-tracker-980c7.appspot.com",
+    messagingSenderId: "450251061816",
+    appId: "1:450251061816:web:78b089f62b820fdf7744cd",
+    measurementId: "G-5HN2BDN65W",
+  },
+};
